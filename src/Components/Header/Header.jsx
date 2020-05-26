dada7921c6ae715915646640a3a6c71bbc8d0290
dmmarsh114 <49419067+dmmarsh114@users.noreply.github.com>
2020-05-26 15:23:26 -0400

    adjusted header positioning

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -14,7 +14,7 @@ const Header = (props) => {
         <div id='home' className='header'>
             <div className='headerLayer'></div>
             <div className='titleText'>
-                <h1 style={{ marginBottom: '5px' }}>Daniel Marsh</h1>
+                <h1 style={{ marginBottom: '5px', fontSize: '2.5em' }}>Daniel Marsh</h1>
                 <h3 style={{ marginTop: '0px', marginBottom: '50px' }}>Web Developer</h3>
                 <Tooltip title='View my Github profile!' arrow>
                     <a id='link' href={props.linkedIn} target='blank'><FontAwesomeIcon icon={faGithubSquare} /></a>
